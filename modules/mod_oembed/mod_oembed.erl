0566987fa4f1b3145ef3c9b3910973e763ac6b3d
Arjan Scherpenisse <arjan@scherpenisse.net>
2011-09-15 21:27:25 +0200

    mod_oembed - caching of results; automatically fill title for new media rscs.

--- modules/mod_oembed/mod_oembed.erl
+++ modules/mod_oembed/mod_oembed.erl
@@ -34,6 +34,8 @@
 
 -include_lib("zotonic.hrl").
 
+%% Endpoint for embed.ly oembed service
+-define(OEMBED_ENDPOINT, "http://api.embed.ly/1/oembed?format=json&maxwidth=500&url=").
 
 %% Fantasy mime type to distinguish embeddable html fragments.
 -define(OEMBED_MIME, <<"text/html-oembed">>).
@@ -64,8 +66,6 @@ observe_rsc_update({rsc_update, Id, _OldProps}, {Changed, Props}, Context) ->
                         {oembed_url, EmbedUrl}
                     ],
 
-                                   %% FIXME oembed lookup here..
-
                     case m_media:get(Id, Context) of
                         undefined ->
                             ok = m_media:replace(Id, MediaProps, Context),
@@ -86,7 +86,7 @@ observe_rsc_update({rsc_update, Id, _OldProps}, {Changed, Props}, Context) ->
                     end
             end,
 
-            Props1 = proplists:delete(oembed_urlProps),
+            Props1 = proplists:delete(oembed_url, Props),
             {Changed or EmbedChanged, Props1};
         false ->
             {Changed, Props}
@@ -106,10 +106,24 @@ observe_media_viewer({media_viewer, _Id, Props, _Filename, _Options}, _Context) 
 
 %% @doc Return the filename of a still image to be used for image tags.
 %% @spec observe_media_stillimage(Notification, _Context) -> undefined | {ok, Filename}
-observe_media_stillimage({media_stillimage, _Id, Props}, _Context) ->
+observe_media_stillimage({media_stillimage, Id, Props}, Context) ->
+    ?DEBUG(Id),
+    ?DEBUG(Props),
     case proplists:get_value(mime, Props) of
         ?OEMBED_MIME ->
-            {ok, "lib/images/youtube.jpg"};
+            case m_rsc:p(Id, depiction, Context) of
+                undefined ->
+                    case z_convert:to_list(proplists:get_value(preview_filename, Props)) of
+                        [] ->
+                            {ok, "lib/images/embed.jpg"};
+                        PreviewFile -> {ok, PreviewFile}
+                    end;
+                DepictionProps ->
+                    case z_convert:to_list(proplists:get_value(filename, DepictionProps)) of
+                        [] -> undefined;
+                        Filename -> {ok, Filename}
+                    end
+            end;
         _ ->
             undefined
     end.
@@ -122,7 +136,7 @@ event({submit, {add_video_embed, EventProps}, _TriggerId, _TargetId}, Context) -
     Id = proplists:get_value(id, EventProps),
     Stay = z_convert:to_bool(proplists:get_value(stay, EventProps, false)),
     EmbedUrl = z_context:get_q_validated("oembed_url", Context),
-    ?DEBUG(EmbedUrl),
+
     case Id of
         %% Create a new page
         undefined ->
@@ -133,9 +147,9 @@ event({submit, {add_video_embed, EventProps}, _TriggerId, _TargetId}, Context) -
                 {title, Title},
                 {is_published, true},
                 {category, video},
+                {mime, ?OEMBED_MIME},
                 {oembed_url, EmbedUrl}
             ],
-            ?DEBUG(Props),
             F = fun(Ctx) ->
                 case m_rsc:insert(Props, Context) of
                     {ok, MediaRscId} ->
@@ -165,7 +179,7 @@ event({submit, {add_video_embed, EventProps}, _TriggerId, _TargetId}, Context) -
                     ?ERROR("~p~n~p", [_Error, _Trace]),
                     z_render:growl_error("Could not create the media page.", Context)
             end;
-        
+
         %% Update the current page
         N when is_integer(N) ->
             Props = [
@@ -178,36 +192,68 @@ event({submit, {add_video_embed, EventProps}, _TriggerId, _TargetId}, Context) -
                 {error, _} ->
                     z_render:growl_error("Could not update the page with the new embed code.", Context)
             end
+    end;
+
+%% @doc When entering the embed URL for a new media item, we trigger the detecting early to guess title/description.
+event({postback, {do_oembed, []}, _TriggerId, _TargetId}, Context) ->
+    Url = z_context:get_q(triggervalue, Context),
+    case oembed_request(Url, Context) of
+        error ->
+            z_render:growl_error(?__("Invalid media URL", Context), Context);
+        Json ->
+            %% Fill title
+            z_context:add_script_page(["$('#oembed-title').val('", proplists:get_value(title, Json, []), "');"], Context),
+            %% And summary
+            z_context:add_script_page(["$('#oembed-summary').val('", proplists:get_value(description, Json, []), "');"], Context),
+            z_render:growl("Detected media item", Context)
     end.
-    
+
 
 %%====================================================================
 %% support functions
 %%====================================================================
 
 %% Fetch or create a preview for the movie
-preview_create(MediaId, InsertProps, Context) ->
-    ok.
-%%preview_youtube(MediaId, InsertProps, z_context:prune_for_async(Context)) end);
+preview_create(MediaId, MediaProps, Context) ->
+    case z_convert:to_list(proplists:get_value(oembed_url, MediaProps)) of
+        [] -> ok;
+        Url -> 
+            Json = oembed_request(Url, Context),
+            %% store found properties in the media part of the rsc
+            ok = m_media:replace(MediaId, [{oembed, Json} | MediaProps], Context),
+            case proplists:lookup(thumbnail_url, Json) of
+                {thumbnail_url, ThumbUrl} ->
+                    ?DEBUG("downloading.."),
+                    {CT, ImageData} = get_thumbnail(ThumbUrl, Context),
+                    {ok, _} = m_media:save_preview(MediaId, ImageData, CT, Context);
+                _ ->
+                    nop
+            end,
+            ?DEBUG(Json)
+    end.
 
-% @doc Fetch the preview image of a youtube video. The preview is located at: http://img.youtube.com/vi/[code]/0.jpg
-% @todo Make this more robust wrt http errors.
-%% preview_youtube(MediaId, InsertProps, Context) ->
-%%     case z_convert:to_list(proplists:get_value(video_embed_code, InsertProps)) of
-%%         [] -> nop;
-%%         Embed ->
-%%             case re:run(Embed, "youtube(\-nocookie)?\\.com/v/([^\?\"'&]+)", [{capture,[2],list}]) of
-%%                 {match, [Code]} ->
-%%                     Url = "http://img.youtube.com/vi/"++Code++"/0.jpg",
-%%                     case http:request(Url) of
-%%                         {ok, {_StatusLine, _Header, Data}} ->
-%%                             %% Received the preview image, move it to a file.
-%%                             m_media:save_preview(MediaId, Data, "image/jpeg", Context);
-%%                         {error, _Reason} ->
-%%                             %% Too bad - no preview available - ignore for now (see todo above)
-%%                             nop
-%%                     end;
-%%                 _ ->
-%%                     nop
-%%             end
-%%     end.
+
+%% @doc Perform OEmbed discovery on a given URL.
+%% @spec oembed_request(string(), #context{}) -> [{Key, Value}]
+oembed_request(Url, Context) ->
+    F = fun() ->
+                %% Use embed.ly service...
+                JsonUrl = ?OEMBED_ENDPOINT ++ z_utils:url_encode(Url),
+                {ok, {{_, 200, _}, _Headers, Body}} = http:request(get, {JsonUrl, []}, [], []),
+                z_convert:convert_json(mochijson2:decode(Body))
+        end,
+    z_depcache:memo(F, {oembed, Url}, 3600, Context).
+
+
+
+%% @doc Given a thumbnail URL, download it and return the content type plus image data pair.
+get_thumbnail(ThumbUrl, Context) ->
+    F = fun() ->
+                {ok, {{_, 200, _}, Headers, ImageData}} = http:request(get, {z_convert:to_list(ThumbUrl), []}, [], []),
+                CT = case proplists:lookup("content-type", Headers) of
+                         {"content-type", C} -> C;
+                         _ -> "image/jpeg"
+                     end,
+                {CT, ImageData}
+        end,
+    z_depcache:memo(F, {oembed_thumbnail, ThumbUrl}, 3600, Context).
--- modules/mod_oembed/templates/_admin_edit_media.tpl
+++ modules/mod_oembed/templates/_admin_edit_media.tpl
@@ -1,5 +1,9 @@
-{% if medium.video_embed_service %}
+{% if medium.oembed_url %}
 	<p class="clear">
-	{% media medium %}
+        {% if medium.oembed.html %}
+        {{ medium.oembed.html }}
+        {% else %}
+        {% image medium width=500 %}
+        {% endif %}
 	</p>
 {% endif %}
